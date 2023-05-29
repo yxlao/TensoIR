9d3d1d650dab392099c6b426b23121564c5a49d7
Yixing Lao <yixing.lao@gmail.com>
2023-05-30 00:09:13 +0800

    num test working

--- dataLoader/ord.py
+++ dataLoader/ord.py
@@ -19,7 +19,32 @@ class ObjectRelightingDataset(Dataset):
 
 
 def main():
-    pass
+    scene_name = "antman"
+
+    # Only the "test" folder is required.
+    ord_root = Path.home() / "research" / "object-relighting-dataset"
+    scene_dir = ord_root / "dataset" / scene_name / "test"
+
+    # Load the training set: {scene_dir}/inputs.
+    inputs_dir = scene_dir / "inputs"
+    train_camera_paths = sorted(inputs_dir.glob("camera_*.txt"))
+    train_im_rgb_paths = sorted(inputs_dir.glob("image_*.png"))
+    train_im_mask_paths = sorted(inputs_dir.glob("mask_*.png"))
+    num_train = len(train_camera_paths)
+    assert num_train == len(train_camera_paths)
+    assert num_train == len(train_im_rgb_paths)
+    assert num_train == len(train_im_mask_paths)
+
+    # Load test set: {scene_dir}.
+    test_camera_paths = sorted(scene_dir.glob("gt_camera_*.txt"))
+    test_im_rgb_paths = sorted(scene_dir.glob("gt_image_*.png"))
+    test_im_mask_paths = sorted(scene_dir.glob("gt_mask_*.png"))
+    num_test = len(test_camera_paths)
+    assert num_test == 9
+    assert num_test == len(test_camera_paths)
+    assert num_test == len(test_im_rgb_paths)
+    assert num_test == len(test_im_mask_paths)
+
 
 
 if __name__ == "__main__":
