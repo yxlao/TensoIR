4901a0a2e547be7635f4678824d9ba9094c76f7d
Yixing Lao <yixing.lao@gmail.com>
2023-05-31 01:50:30 +0800

    add docker_patch

---
+++ docker_patch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install tqdm scikit-image opencv-python configargparse lpips imageio-ffmpeg kornia lpips tensorboard loguru plyfile
+pip install setuptools==59.5.0 imageio==2.11.1 yapf==0.30.0
