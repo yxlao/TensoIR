c8d37d2fd7a06d57aed61be5540b7820d9b0e651
Yixing Lao <yixing.lao@gmail.com>
2023-06-03 21:25:44 +0800

    init prepare code

--- eval/prepare_eval.py
+++ eval/prepare_eval.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import json
+import shutil
+import camtools as ct
 
 script_dir = Path(__file__).parent.absolute()
 
@@ -7,6 +9,8 @@ script_dir = Path(__file__).parent.absolute()
 def prepare_ord_relight():
     with open(script_dir / "ord_relight.json", "r") as f:
         ord_relight_list = json.load(f)
+
+    # Check.
     for eval_item in ord_relight_list:
         gt_path = Path(eval_item["gt_path"])
         pd_src_path = Path(eval_item["pd_src_path"])
@@ -15,10 +19,21 @@ def prepare_ord_relight():
         if not pd_src_path.exists():
             print(f"{pd_src_path} does not exist.")
 
+    # Prepare.
+    # Copy pd_src_path -> pd_dst_path, mkdir if not exists.
+    for eval_item in ord_relight_list:
+        pd_src_path = Path(eval_item["pd_src_path"])
+        pd_dst_path = Path(eval_item["pd_dst_path"])
+        pd_dst_path.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy(pd_src_path, pd_dst_path)
+        print(f"Copy {pd_src_path} -> {pd_dst_path}")
+
 
 def prepare_ord_nvs():
     with open(script_dir / "ord_nvs.json", "r") as f:
         ord_nvs_list = json.load(f)
+
+    # Check.
     for eval_item in ord_nvs_list:
         gt_path = Path(eval_item["gt_path"])
         pd_src_path = Path(eval_item["pd_src_path"])
@@ -26,6 +41,22 @@ def prepare_ord_nvs():
             print(f"{gt_path} does not exist.")
         if not pd_src_path.exists():
             print(f"{pd_src_path} does not exist.")
+
+    # Prepare.
+    # Copy pd_src_path -> pd_dst_path, mkdir if not exists.
+    # Instead of simple copying, we need to crop the image by the left 1/3.
+    for eval_item in ord_nvs_list:
+        pd_src_path = Path(eval_item["pd_src_path"])
+        pd_dst_path = Path(eval_item["pd_dst_path"])
+
+        pd_dst_path.parent.mkdir(parents=True, exist_ok=True)
+        im_pd = ct.io.imread(pd_src_path)
+        src_shape = im_pd.shape
+        im_pd = im_pd[:, :src_shape[1] // 3, :]
+        dst_shape = im_pd.shape
+        ct.io.imwrite(im_pd, pd_dst_path)
+        print(f"Copy {pd_src_path} -> {pd_dst_path}, "
+              f"shape: {src_shape} -> {dst_shape}")
 
 
 def main():
