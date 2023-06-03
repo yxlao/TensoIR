abddb9ed842c2d4c7a96147783c3729d4d7647fd
Yixing Lao <yixing.lao@gmail.com>
2023-06-03 22:16:48 +0800

    prepare all

--- eval/prepare_eval.py
+++ eval/prepare_eval.py
@@ -101,7 +101,7 @@ def main():
     prepare_nvs(script_dir / "ord_nvs.json")
 
     prepare_relight(script_dir / "synth4relight_relight.json")
-    # prepare_nvs(script_dir / "synth4relight_nvs.json")
+    prepare_nvs(script_dir / "synth4relight_nvs.json")
 
 
 if __name__ == "__main__":
