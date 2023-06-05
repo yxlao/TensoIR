d9a599f7594bd2e11646079547916e73e0017d16
Yixing Lao <yixing.lao@gmail.com>
2023-06-05 17:23:30 +0800

    gen_commands support searching multiple exp dirs

--- gen_commands.py
+++ gen_commands.py
@@ -1,34 +1,34 @@
 from pathlib import Path
 
 _datasets_scenes = [
-    ("ord", "antman"),
-    ("ord", "apple"),
-    ("ord", "chest"),
-    ("ord", "gamepad"),
-    ("ord", "ping_pong_racket"),
-    ("ord", "porcelain_mug"),
-    ("ord", "tpiece"),
-    ("ord", "wood_bowl"),
-    ("synth4relight_subsampled", "air_baloons"),
-    ("synth4relight_subsampled", "chair"),
-    ("synth4relight_subsampled", "hotdog"),
-    ("synth4relight_subsampled", "jugs"),
-    # ("bmvs", "bear"),
-    # ("bmvs", "clock"),
-    # ("bmvs", "dog"),
-    # ("bmvs", "durian"),
-    # ("bmvs", "jade"),
-    # ("bmvs", "man"),
-    # ("bmvs", "sculpture"),
-    # ("bmvs", "stone"),
-    # ("dtu", "scan37"),
-    # ("dtu", "scan40"),
-    # ("dtu", "scan55"),
-    # ("dtu", "scan63"),
-    # ("dtu", "scan65"),
-    # ("dtu", "scan69"),
-    # ("dtu", "scan83"),
-    # ("dtu", "scan97"),
+    # ("ord", "antman"),
+    # ("ord", "apple"),
+    # ("ord", "chest"),
+    # ("ord", "gamepad"),
+    # ("ord", "ping_pong_racket"),
+    # ("ord", "porcelain_mug"),
+    # ("ord", "tpiece"),
+    # ("ord", "wood_bowl"),
+    # ("synth4relight_subsampled", "air_baloons"),
+    # ("synth4relight_subsampled", "chair"),
+    # ("synth4relight_subsampled", "hotdog"),
+    # ("synth4relight_subsampled", "jugs"),
+    ("bmvs", "bear"),
+    ("bmvs", "clock"),
+    ("bmvs", "dog"),
+    ("bmvs", "durian"),
+    ("bmvs", "jade"),
+    ("bmvs", "man"),
+    ("bmvs", "sculpture"),
+    ("bmvs", "stone"),
+    ("dtu", "scan37"),
+    ("dtu", "scan40"),
+    ("dtu", "scan55"),
+    ("dtu", "scan63"),
+    ("dtu", "scan65"),
+    ("dtu", "scan69"),
+    ("dtu", "scan83"),
+    ("dtu", "scan97"),
 ]
 
 
@@ -49,14 +49,28 @@ def get_latest_checkpoint_path(dataset, scene):
     ]
     if len(exp_dirs) == 0:
         raise ValueError(f"No experiment directory found for {dataset_scene}.")
-    exp_dir = sorted(exp_dirs)[-1]
-    ckpt_dir = exp_dir / "checkpoints"
-    ckpt_paths = [
-        p for p in ckpt_dir.iterdir() if p.is_file() and p.suffix == ".th"
-    ]
-    if len(ckpt_paths) == 0:
-        raise ValueError(f"No checkpoint found for {dataset_scene}.")
-    ckpt_path = sorted(ckpt_paths)[-1]
+
+    max_ckpt_iter = 0
+    ckpt_path = None
+
+    for exp_dir in exp_dirs:
+        ckpt_dir = exp_dir / "checkpoints"
+        ckpt_paths = sorted([
+            p for p in ckpt_dir.iterdir() if p.is_file() and p.suffix == ".th"
+        ])
+        print(ckpt_paths)
+        if len(ckpt_paths) == 0:
+            continue
+        else:
+            ckpt_iter = int(ckpt_paths[-1].stem.split("_")[-1])
+            if ckpt_iter > max_ckpt_iter:
+                max_ckpt_iter = ckpt_iter
+                ckpt_path = ckpt_paths[-1]
+
+    if max_ckpt_iter == 0:
+        raise ValueError(f"No checkpoint found for {dataset_scene}, "
+                         f"searched {exp_dirs}.")
+
     return ckpt_path
 
 
@@ -141,11 +155,12 @@ def gen_commands(dataset, scene):
 def main():
     all_cmds = []
     for dataset, scene in _datasets_scenes:
-        train_cmd, render_cmd, relight_cmd, render_relight_cmd = gen_commands(dataset, scene)
+        train_cmd, render_cmd, relight_cmd, render_relight_cmd = gen_commands(
+            dataset, scene)
         print("######################")
         print(f"{dataset}_{scene}:")
-        print(render_relight_cmd)
-        all_cmds.append(render_relight_cmd)
+        print(render_cmd)
+        all_cmds.append(render_cmd)
 
     with open("commands.txt", "w") as f:
         f.write("\n".join(all_cmds))
